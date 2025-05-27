e25e2fcd14b9b04b9901d03c15eb98775b0e7e0c
ahsan <ahsan@ahsan.ahsan>
2025-05-27 10:46:41 +0530

    comment out

--- dreload.sh
+++ dreload.sh
@@ -7,8 +7,8 @@ source .env
 # echo "sudo docker stop $CONTAINER_NAME"
 sudo docker stop $CONTAINER_NAME;
 sudo docker rm $CONTAINER_NAME;
-sudo docker rmi $IMAGE_NAME:$VERSION; # Don't Delete Otherwise Conflict may Arise
-sudo rm -rf psqlData; # Not Required Because Actual Data Will Be Deleted
+# sudo docker rmi $IMAGE_NAME:$VERSION; # Don't Delete Otherwise Conflict may Arise
+# sudo rm -rf psqlData; # Not Required Because Actual Data Will Be Deleted
 echo y | sudo docker system prune;
 sudo docker compose up -d;
 echo "sudo docker exec -it $CONTAINER_NAME psql -U $PG_USER $PG_DB_NAME";
