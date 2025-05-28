6ccd37c84503e6dc5f122ce7edce0b103424add2
ahsan <ahsan@ahsan.ahsan>
2025-05-28 14:20:45 +0530

    added SERVICE_NAME

--- dreload.sh
+++ dreload.sh
@@ -2,6 +2,7 @@
 
 # Load variables from .env
 source .env
+export $(grep -v '^#' .env | xargs)
 
 # Use the variables
 # echo "sudo docker stop $CONTAINER_NAME"
@@ -10,5 +11,10 @@ sudo docker rm $CONTAINER_NAME;
 sudo docker rmi $IMAGE_NAME:$VERSION;
 # sudo rm -rf psqlData; # Not Required Because Actual Data Will Be Deleted
 echo y | sudo docker system prune;
-sudo docker compose up -d;
+
+# Replace __SERVICE_NAME__ manually using sed, then envsubst for the rest
+sed "s/__SERVICE_NAME__/${SERVICE_NAME}/g" docker-compose.yml | envsubst > docker-compose.template.yml
+
+sudo docker compose -f ./docker-compose.template.yml up -d;
 echo "sudo docker exec -it $CONTAINER_NAME psql -U $PG_USER $PG_DB_NAME";
+sudo rm -rf  docker-compose.template.yml;
